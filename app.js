6c5d3be427aa2e47c2689e7774c37710c4eaee52
Rishabh <rishabhv2100@gmail.com>
2025-08-03 17:48:36 +0530

    Root route fix

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user.js');
+const wrapAsync = require('../utils/wrapAsync.js');
 
 const listingsRouter = require('./routes/listing.js');
 const reviewsRouter = require('./routes/review.js');
